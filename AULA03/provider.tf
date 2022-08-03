4dd6c62afbc85fed92eacec4be964f13399b467b
Anderson Bispo <anderson.bispo@4linux.com.br>
2022-08-03 10:21:45 -0300

    provider.tf

---
+++ AULA03/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.31.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = "/Users/anderson/Documents/gcp-svc/terraform-4linux-8209-c8557147cf33.json"
+  project     = "terraform-4linux-8209"
+  region      = "us-central1"
+}
