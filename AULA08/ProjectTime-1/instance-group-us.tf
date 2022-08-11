0f7fa322d2fcc0fe8eb5e9a05abee411a0ef3f40
Anderson Bispo <anderson.bispo@4linux.com.br>
2022-08-11 09:16:33 -0300

    fix group

--- AULA08/ProjectTime-1/instance-group-us.tf
+++ AULA08/ProjectTime-1/instance-group-us.tf
@@ -11,17 +11,3 @@ resource "google_compute_region_instance_group_manager" "appserver-us" {
 
   target_size = 0
 }
-
-resource "google_compute_region_instance_group_manager" "appserver-br" {
-  name = "appserver-br"
-
-  base_instance_name         = "webapps"
-  region                     = var.instance_group_regions[1]
-  /* distribution_policy_zones  = ["us-central1-a", "us-central1-f"] */
-
-  version {
-    instance_template = google_compute_instance_template.default.self_link
-  }
-
-  target_size = 0
-}
