cb2820cf77aebbff8ebce193ba79d0adfd091b1c
Anderson Bispo <anderson.bispo@4linux.com.br>
2022-08-12 10:39:30 -0300

    instancia

--- CODIGOS-AULAS/AULA10/tf-modules/instancia/main.tf
+++ CODIGOS-AULAS/AULA10/tf-modules/instancia/main.tf
@@ -1,15 +1,15 @@
 resource "google_compute_instance" "vm_teste" {
-  name         = "vm1"
+  name         = var.vm_name
   machine_type = "e2-micro"
-  zone         = "us-central1-a"
+  zone         = var.vm_zone
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.vm_image
     }
   }
 
   network_interface {
-    network = module.vpc.vpc_self_link
+    network = var.vm_network
   }
 }
